0a16ecf75c46c5144afc36b94583c1446735d8aa
vexnatic <59765742+fhdgfhdggfh@users.noreply.github.com>
2025-04-24 01:09:19 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyASvWugWbCjIHspHbO1VuHh2MnhNasVkEk",
+  authDomain: "mein-shop-797e4.firebaseapp.com",
+  projectId: "mein-shop-797e4",
+  storageBucket: "mein-shop-797e4.firebasestorage.app",
+  messagingSenderId: "1062564560079",
+  appId: "1:1062564560079:web:e243543f08fe1f6d178bba",
+  measurementId: "G-X4H8F9G4TE"
+};
